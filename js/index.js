26974089b5864a0d15db04472a597802fc092888
Melissa Hoff <luv2read.hoff@gmail.com>
2016-08-10 12:01:40 -0400

    Added food.html and worked on js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
-function buttonCLick (sender)
-{
-	
+var metus = {
+	name: "Bob",
+	stressLevel: 10,
+	images: {"../img/metus1.jpg", "../img/metus2.jpg", "../img/metus3.jpg", 
+	"../img/metus4.jpg", "../img/metus5.jpg"},
+	pickImg: function (){
+		var img;
+		//choose img
+		var index = (stressLevel + (stressLevel%2))/2 - 1
+		img = images[index];
+		return img;
+	}
 }
+
+
 
