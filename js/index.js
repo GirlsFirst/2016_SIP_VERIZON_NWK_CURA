95451f63e38f6b96ac3991d4e2b7390b76578929
Melissa Hoff <luv2read.hoff@gmail.com>
2016-08-11 09:33:22 -0400

    added progress bar to food page

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ var stressLevel = 10;
 //keeps track of how many times the metus has been fed
 var ate = 0;
 
+// chooses the appropriate metus image for the current stress level
 function pickImg(){
 	var img;
 	//choose img
@@ -11,51 +12,72 @@ function pickImg(){
 	document.getElementById("metus").setAttribute("src",img);
 }
 
-function updateProgress(){
-	var stress = stressLevel * 10;
-	document.getElementById("progressBar").setAttribute("style","width:" + stress +"%");
+//sets progress bar initially
+function setBar()
+{
+	var elem = document.getElementById("myBar");
+	var width = stressLevel*10
+	elem.style.width = width + '%';
 }
 
+//updates the progress bar by moving it down 10%
+//  and updates the metus image
+function updateProgress() {
+    var elem = document.getElementById("myBar"); 
+    var width = (stressLevel + 1)*10;	//current width of the bar
+    var end = width - 10;	//ending width of the bar
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width <= end) {
+            clearInterval(id);
+        } else {
+            width--; 
+            elem.style.width = width + '%'; 
+        }
+    }
+    pickImg();
+}
 
+//decreases stressLevel the first 5 times the metus eats
 function ateFood()
 {
 	if (ate < 5)
 	{
 		stressLevel--;
 		updateProgress();
-		pickImg();
 		ate++;
 	}
 }
 
+//gives information and calls ateFood() to decrease stressLevel
 function chocolateInfo()
 {
 	var info = "chocolateInfo";
 	confirm(info);
 	ateFood();
-	updateProgress();
 }
 
+//gives information and calls ateFood() to decrease stressLevel
 function blueberriesInfo()
 {
 	var info = "blueberriesInfo";
 	confirm(info);
 	ateFood();
-	updateProgress();
 }
 
+//gives information and calls ateFood() to decrease stressLevel
 function avocadoInfo()
 {
 	var info = "avocadoInfo";
 	confirm(info);
 	ateFood();
-	updateProgress();
 }
 
+//gives information and calls ateFood() to decrease stressLevel
 function vegesInfo()
 {
 	var info = "vegesInfo";
 	confirm(info);
 	ateFood();
-	updateProgress();
 }
+
